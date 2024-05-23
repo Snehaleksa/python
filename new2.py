22867ed84b788a1e5e4efbb2c88785489f490ee6
Snehaleksa <snehaleksa2016@gmail.com>
2024-05-23 03:43:03 +0100

    message

--- new2.py
+++ new2.py
@@ -1,2 +1,24 @@
-a=[1,2,3,4]
+a="hello world"
 print(a)
+print(type(a))
+print(len(a))
+
+string1="abc"
+string2="def"
+print(string1+' '+string2)
+print(string1+'4'+string2)
+
+b="abc"
+print(b*3)
+
+c="hello World"
+print(c.lower())
+print(c.upper())
+x=c.count("l")
+print(x)
+print(c.find ("world"))
+replaced_c=c.replace("hello","hai")
+print(replaced_c)
+
+print(10>9)
+print(1>2)

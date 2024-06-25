575e68da9ca558f747104796692ad8204490049b
Snehaleksa <snehaleksa2016@gmail.com>
2024-06-25 12:30:28 +0100

    'message'

--- logic2.py
+++ logic2.py
@@ -40,14 +40,6 @@ for i in range(0,4):
         else:
             print(' ',end=' ')  
 
-    print() 
-
-k=2
-for i in range(0,11):
-    for j in range(0,5):
-      if i==0 and j==0:
-          print(k,end=' ')
-           
     print()           
 
 

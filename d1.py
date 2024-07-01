80cbddb681f3cbd29f3cff9eafcda2781a65ff43
Snehaleksa <snehaleksa2016@gmail.com>
2024-07-01 11:28:03 +0100

    'message'

--- d1.py
+++ d1.py
@@ -1,1 +1,9 @@
-        
+nums=input('Enter the list(seperate with spaces)') 
+nums=list(map(int,nums.split()))           
+target=int(input('Enter a number'))
+for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+ nums[j]==target:
+             result=i,j
+             print([int(d)for d in result])   
+                 
--- logic5.py
+++ logic5.py
@@ -3,11 +3,13 @@ a=s[::-1]
 print(a)
 
 
-l=[]
-list=[2,7,11,15]
-b=list.index(2)
-c=list.index(7)
-l.append(b),l.append(c)
-print(l)
-
+nums=input('Enter the list(seperate with spaces)') 
+nums=list(map(int,nums.split()))           
+target=int(input('Enter a number'))
+for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]+ nums[j]==target:
+             result=i,j
+             print([int(d)for d in result])   
+                 
 
